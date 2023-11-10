48499082d01cadf44d7fd057f064fdf542dc487b
maris-rozenstoks <146951342+maris-rozenstoks@users.noreply.github.com>
2023-11-10 15:58:33 +0200

    Constrains
    Working on constraints. Text on multiple lines...

--- PokemonApp/Controller/PokemonDetailViewController.swift
+++ PokemonApp/Controller/PokemonDetailViewController.swift
@@ -17,15 +17,20 @@ class PokemonDetailViewController: UIViewController {
     @IBOutlet weak var pokemonsArtist: UILabel!
     @IBOutlet weak var pokemonsType: UILabel!
     @IBOutlet weak var pokemonsText: UILabel!
-    
+    @IBOutlet weak var pokemonsHp: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         //         Do any additional setup after loading the view.
         
         pokemonsImage.sd_setImage(with: URL(string: pokemon!.imageURL))
         pokemonsName.text = pokemon?.name
-        pokemonsArtist.text =  pokemon?.hp
+        pokemonsArtist.text =  pokemon?.artist
         pokemonsType.text = pokemon?.supertype
+        if let hp = pokemon?.hp {
+                    pokemonsHp.text = "HP: \(hp)"
+                } else {
+                    pokemonsHp.text = "HP: N/A"
+                }
         pokemonsText.text = pokemon?.text?.joined(separator: "\n")
     }
     
